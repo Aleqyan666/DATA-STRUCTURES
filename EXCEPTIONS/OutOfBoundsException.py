a37ccf8f6461d276735255e8fa1b4f917826e21a
Hayk Alekyan <109866909+Aleqyan666@users.noreply.github.com>
2024-09-06 13:42:10 +0400

    Delete EXCEPTIONS directory

--- EXCEPTIONS/OutOfBoundsException.py
+++
@@ -1,6 +0,0 @@
-class OutOfBoundsException(Exception):
-    """Custom exception when an index or value is out of bounds."""
-    
-    def __init__(self, index):
-        self.message = f"Index '{index}' is out of bounds." 
-        super().__init__(self.message) 
