e50fa141ee8cc0eb5f23276b72b5188d85380933
Aleqyan666 <haykalekyan@gmail.com>
2024-09-06 13:34:01 +0400

    Another naming chnage

--- EXCEPTIONS/OutOfBoundsException.py
+++ EXCEPTIONS/OutOfBoundsException.py
@@ -2,5 +2,5 @@ class OutOfBoundsException(Exception):
     """Custom exception for when an index or value is out of bounds."""
     
     def __init__(self, index):
-        self.message = f"Index '{index}' is out of bounds."
+        self.message = f"Index '{index}' is out of bounds." 
         super().__init__(self.message) 
